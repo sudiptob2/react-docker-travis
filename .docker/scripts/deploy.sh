e0574c8a4b90bd7e4cb052813e4cd1fa4338058e
Sudipto Baral <sudipto.baral@bracits.com>
2021-07-26 14:36:08 +0600

    test push

--- .docker/scripts/deploy.sh
+++ .docker/scripts/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-docker build -t react-docker-travis:prod -f ./.docker/prod/Dockerfile .
-
 docker login --username $HEROKU_DOCKER_USERNAME --password $HEROKU_API_KEY registry.heroku.com
-
+docker build -t react-docker-travis:prod -f ./.docker/prod/Dockerfile .
 docker tag react-docker-travis:prod registry.heroku.com/sudipotb2-react-docker-travis/web
 docker push registry.heroku.com/sudipotb2-react-docker-travis/web
 heroku container:release -a sudipotb2-react-docker-travis web
